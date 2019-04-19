313ecfd0e56d24736366a9f340e77c7a46009c90
Sasankh Munukutla <25861686+SasankhM@users.noreply.github.com>
2019-04-18 20:46:32 -0700

    Update sentiment-analysis.py

--- sentiment-analysis.py
+++ sentiment-analysis.py
@@ -1,1 +1,18 @@
 import numpy as np
+import json
+import sys
+import urllib
+import os
+import twitter
+import tweepy
+
+def main():
+    url = urllib.request("http://search.twitter.com/search.json?q="+"refugee")
+    data = json.load(url)
+    print (len(data), "tweets")
+
+    for tweet in data["results"]:
+        print (tweet["text"])
+
+if __name__ == '__main__':
+    main()
