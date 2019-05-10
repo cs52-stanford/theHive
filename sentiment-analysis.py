80837b2ea58f0ae3f0b6ca6cfbd17ce1dcb81485
Sasankh Munukutla <25861686+SasankhM@users.noreply.github.com>
2019-05-09 19:12:38 -0700

    Sentiment Updated

--- sentiment-analysis.py
+++ sentiment-analysis.py
@@ -1,14 +1,21 @@
 import numpy as np
+import pandas as pd
 import csv
 from concurrent.futures import ThreadPoolExecutor
 import threading
 import tweepy
 from tweepy import OAuthHandler
+import json
 import pyrebase
 import copy
 import datetime
 import firebase_admin
 from firebase_admin import credentials, db
+import re
+import os
+import psutil
+import ast
+import numpy as np
 
 kRequests = 450 #450 searches per 15/mins limit
 queries = ['#refugee', '#immigrants','#withrefugees', 'USA for UNHCR',
@@ -33,59 +40,79 @@ queries = ['#refugee', '#immigrants','#withrefugees', 'USA for UNHCR',
             'border wall', 'illegal border crossing']
 
 def main():
+    process = psutil.Process(os.getpid())
     credentials_arr = read_credentials() # returns array of dicts, each dict represents one login
     apis = get_apis(credentials_arr) # returns array of api objects
 
     csvOutput = open('saved-tweets.csv', 'a')
     csvWriter = csv.writer(csvOutput)
 
-    ref = db.reference()
-    makeQueries(apis, ref)
+    # ref = db.reference()
+    # makeQueries(apis, ref)
+    retrieveData()
+    print(process.memory_info().rss)
+
+def retrieveData():
+    ref = db.reference('Tweet-More')
+    # tweets = ast.literal_eval(ref.get())
+    # array = [np.array(tweet) for tweet in tweets["tweet"]]
+    result = ref.get()
+    arr = np.array(list(result.items()))
+    print(arr)
+
+
+
 
 def limit_handled(cursor, index):
     while True:
         try:
             yield cursor.next()
-        except tweepy.TweepError:
+        except tweepy.RateLimitError:
             print("Limit reached on api " + index)
-            time.sleep(60 * 15)
             continue
         except StopIteration:
             break
 
 def runQuery(index, api, ref):
     print("Task executed on thread " + str(index))
-    for tweet in limit_handled(tweepy.Cursor(api.search, q="#refugee", lang="en", geocode="39.8,-95.583068847656,2500km").items(), index):
+    # print("Rate limit status for api " + str(index) + ": " + api.rate_limit_status())
+    for tweet in limit_handled(tweepy.Cursor(api.search, q="#refugee", lang="en", geocode="39.8,-95.583068847656,2500km", tweet_mode="extended").items(), index):
         # print (tweet.created_at, tweet.text, tweet.user.location)
         # csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8')])
-        tweet_ref = ref.child('Tweets')
+        tweet_ref = ref.child('Tweet-More')
         new_tweet = tweet_ref.push()
-        if (tweet.place):
-            tweetCountry = tweet.place.country
-            print(tweetCountry)
+        # if (tweet.place):
+        #     tweetCountry = tweet.place.country
+        #     print(tweetCountry)
+        if (tweet.retweeted):
+            print(str(tweet.retweeted_status))
+        # print(tweet.full_text)
         new_tweet.set({
             'Date': str(tweet.created_at),
-            'Tweet': tweet.text,
+            'Tweet': str(tweet.full_text),
             'User location': tweet.user.location, #where the user is based, not where they tweeted from
             #'Tweet coordinates': tweet.place.coordinates,
             # 'Country': tweet.place.country,
             'Retweets': tweet.retweet_count,
+            'Replies': tweet.reply_count,
             'Liked': tweet.favorite_count,
             'User': tweet.user.name,
             'Handle': tweet.user.screen_name,
             'Followers': tweet.user.followers_count,
             'Total Tweets by user': tweet.user.statuses_count,
+            # 'Tweet URL': re.search("(?P<url>https?://[^\s]+)", tweet.text).group("url"),
             # 'Tweets Per Day by User': tweet.user.statuses_count / (datetime.utcnow() - datetime(tweet.user.created_at)).days()
         })
     print("Task finished on thread " + str(index))
 
 def makeQueries(apis, ref):
-    print("Starting threads...")
-    executor = ThreadPoolExecutor(max_workers = len(apis))
+    # print("Starting threads...")
+    # executor = ThreadPoolExecutor(max_workers = len(apis))
     for index, api in enumerate(apis):
-        print("Thread " + str(index) + " started")
-        executor.submit(runQuery, index, api, ref)
-        print("Thread " + str(index) + " scheduled")
+        # print("Rate limit status for api " + str(index) + ": " + str(api.rate_limit_status()))
+        print("Api " + str(index) + " started")
+        runQuery(index, api, ref)
+        print("Api " + str(index) + " finished")
 
 def get_apis(credentials):
     apis = []
