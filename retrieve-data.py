bc12f9ba1bfd3fb167a478f706f6d6d60e397417
Sasankh Munukutla <25861686+SasankhM@users.noreply.github.com>
2019-05-30 17:11:59 -0700

    Influencers identified and saved to FB

--- retrieve-data.py
+++ retrieve-data.py
@@ -27,9 +27,9 @@ def main():
     retrieveData()
 
 def retrieveData():
-    ref = db.reference('Tweets-Real')
+    ref = db.reference('/Tweets-Folder/' + str(datetime.date.today()) + '/')
     result = ref.get()
-    data = pd.DataFrame(columns=['Date', 'Tweet','Tweet latitude', 'Tweet longitude', 'User','User location', 'Retweets', 'Replies', 'Liked', 'User', 'Handle', 'Followers', 'Total Tweets by user'])
+    data = pd.DataFrame(columns=['Date', 'Tweet','Tweet latitude', 'Tweet longitude', 'User','User location', 'Retweets', 'Replies', 'Liked', 'Handle', 'Followers', 'Total Tweets by user'])
     for i,key in enumerate(result.keys()):
         data.loc[i] = (result[key]['Date'] if 'Date' in result[key] else np.NaN,
         result[key]['Tweet'] if 'Tweet' in result[key] else np.NaN,
@@ -40,11 +40,43 @@ def retrieveData():
         result[key]['Retweets'] if 'Retweets' in result[key] else np.NaN,
         result[key]['Replies'] if 'Replies' in result[key] else np.NaN,
         result[key]['Liked'] if 'Liked' in result[key] else np.NaN,
-        result[key]['User'] if 'User' in result[key] else np.NaN,
         result[key]['Handle'] if 'Handle' in result[key] else np.NaN,
         result[key]['Followers'] if 'Followers' in result[key] else np.NaN,
         result[key]['Total Tweets by user'] if 'Total Tweets by user' in result[key] else np.NaN)
-    print(data[data['Followers'] == data['Followers'].max()])
+    data.fillna(0)
+    data['Influencer-Score'] = (data['Followers'] * (data['Retweets']+1) * data['Total Tweets by user'] * (data['Liked']+1)) / 10**9
+    influencers = data.loc[data['Influencer-Score'] >= 1]
+
+    influencer_ref = db.reference('Influencers/')
+    for index,row in influencers.iterrows():
+        existingChild = ref.child(row['Handle']).get()
+        if(existingChild):
+            if(existingChild['Influencer-Score'] < row['Influencer-Score']):
+                existingChild.update({
+                    'User': row['User'],
+                    'User location': row['User location'],
+                    'Handle': row['Handle'],
+                    'Latitude': row['Tweet latitude'],
+                    'Longitude': row['Tweet longitude'],
+                    'Followers': row['Followers'],
+                    'Total Tweets by user': row['Total Tweets by user'],
+                    'Tweet': row['Tweet'],
+                    'Influencer-Score': row['Influencer-Score'],
+                })
+        else:
+            newInfluencer = influencer_ref.child(row['Handle'])
+            newInfluencer.update({
+                'User': row['User'],
+                'User location': row['User location'],
+                'Handle': row['Handle'],
+                'Latitude': row['Tweet latitude'],
+                'Longitude': row['Tweet longitude'],
+                'Followers': row['Followers'],
+                'Total Tweets by user': row['Total Tweets by user'],
+                'Tweet': row['Tweet'],
+                'Influencer-Score': row['Influencer-Score'],
+            })
+
 
 if __name__ == '__main__':
     main()
